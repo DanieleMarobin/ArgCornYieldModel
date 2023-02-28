a79617887d390181f9e941dfc7472a4e14530d80
DanieleMarobin <daniele.marobin@hotmail.com>
2023-02-28 20:58:33 +0100

    Removed sklearn

--- Modeling.py
+++ Modeling.py
@@ -10,10 +10,6 @@ import concurrent.futures
 from itertools import combinations, combinations_with_replacement
 from calendar import isleap
 
-from sklearn.model_selection import TimeSeriesSplit
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_absolute_percentage_error
-
 import GLOBAL as GV
 import Charts as uc
 
@@ -522,58 +518,6 @@ def extract_yearly_ww_variables_old(w_df, var_windows=[], join='inner', drop_na=
     return  out_df
 
 
-def stats_model_cross_validate(X_df, y_df, folds):
-    """
-    This function adds the constant
-
-    So I need to make sure that 'X_df' doesn't contain the constant when passing it as input into this function
-    """
-
-    fo = {'cv_models':[], 'cv_corr':[], 'cv_p':[], 'cv_r_sq':[], 'cv_y_test':[],'cv_y_pred':[], 'cv_MAE':[], 'cv_MAPE':[]}
-       
-    X2_df = sm.add_constant(X_df)
-    
-    for split in folds:        
-        train, test = split[0], split[1]
-
-        max_corr=0
-        if X_df.shape[1]>1:
-            max_corr = np.abs(np.corrcoef(X_df.iloc[train],rowvar=False))
-            max_corr=np.max(max_corr[max_corr<0.999]) 
-        
-        model = sm.OLS(y_df.iloc[train], X2_df.iloc[train]).fit()
-
-        fo['cv_models']+=[model]
-        fo['cv_corr']+=[max_corr]
-        
-        fo['cv_p']+=list(model.pvalues)
-        fo['cv_r_sq']+=[model.rsquared]
-                
-        y_test = y_df.iloc[test]
-        y_pred = model.predict(X2_df.iloc[test])
-        
-        fo['cv_y_test']+=[y_test]
-        fo['cv_y_pred']+=[y_pred]
-        
-        fo['cv_MAE']+=[mean_absolute_error(y_test, y_pred)]
-        fo['cv_MAPE']+=[mean_absolute_percentage_error(y_test, y_pred)]
-        
-    return fo
-
-
-def folds_expanding(model_df, min_train_size=10):    
-    if 'const' in model_df.columns:
-        col_n=len(model_df.columns)
-    else:
-        col_n=len(model_df.columns)+1
-    
-    min_train_size= max(min_train_size,col_n+1) # Obviously cannot run a model if I have 10 points and 10 columns: so I am adding 1 point to the columns size
-    min_train_size= min(min_train_size,len(model_df)-3) # Adjusting for the number of datapoints
-
-    folds_expanding = TimeSeriesSplit(n_splits=len(model_df)-min_train_size, max_train_size=0, test_size=1)
-    folds = []
-    folds = folds + list(folds_expanding.split(model_df))
-    return folds
 
 def print_folds(folds, years, X_df=None):
     '''
@@ -587,11 +531,7 @@ def print_folds(folds, years, X_df=None):
     else: 
         for train, test in folds.split(X_df): print(years[train], "------>", years[test])
 
-def MAPE(y_true, y_pred):
-    return mean_absolute_percentage_error(y_true, y_pred)
 
-def MAE(y_true, y_pred):
-    return mean_absolute_error(y_true, y_pred)
 
 def dm_scaler(df, col_to_rescale, new_min=-100.0, new_max=100.0):
     # I take the abs values because I want to have a summetric range (so that the signs will remain the same)
